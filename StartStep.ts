73f208a2ecc2357b43c7f0a90657ced42bcd314e
FTWinston <ftwinston@gmail.com>
2017-02-15 00:23:18 +0000

    Moved StartStep and StopStep classes into their own files

---
+++ StartStep.ts
@@ -0,0 +1,43 @@
+﻿namespace Cursive {
+    export class StartStep extends Step {
+        constructor(parentProcess: Process, x: number, y: number) {
+            super(null, parentProcess, x, y);
+        }
+        protected writeText(ctx) {
+            ctx.font = '16px sans-serif';
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'middle';
+            ctx.fillStyle = '#0a0';
+            ctx.fillText("Start", this.x - this.radius / 4, this.y);
+        }
+        protected createBodyRegion() {
+            return new Region(
+                function (ctx) {
+                    ctx.moveTo(this.x - this.radius, this.y - this.radius * 0.75);
+                    ctx.lineTo(this.x + this.radius, this.y);
+                    ctx.lineTo(this.x - this.radius, this.y + this.radius * 0.75);
+                    ctx.closePath();
+                }.bind(this),
+                this.drawBody.bind(this),
+                'move'
+            )
+        }
+        protected createCollisionRegion() {
+            return new Region( // twice the normal radius, so that another step can't overlap this one
+                function (ctx) {
+                    ctx.moveTo(this.x - this.radius * 2, this.y - this.radius * 1.5);
+                    ctx.lineTo(this.x + this.radius * 2, this.y);
+                    ctx.lineTo(this.x - this.radius * 2, this.y + this.radius * 1.5);
+                    ctx.closePath();
+                }.bind(this)
+            )
+        }
+        createDanglingReturnPaths() {
+            let returnPath = new ReturnPath(this, null, null, 150, 0);
+            returnPath.onlyPath = true;
+            this.returnPaths.push(returnPath);
+        }
+        protected getInputs() { return null; }
+        protected getOutputs() { return this.parentProcess.inputs; }
+    }
+}
