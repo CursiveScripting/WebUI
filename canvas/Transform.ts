51c1a7ab05a6571edcb7ec8881008f79921280c6
FTWinston <ftwinston@gmail.com>
2018-03-28 15:39:30 +0100

    Replaced project with a create-react-app template

--- canvas/Transform.ts
+++
@@ -1,9 +0,0 @@
-﻿namespace Cursive {
-    export class Transform {
-        constructor(readonly x: number, readonly y: number, readonly angle: number) { }
-        apply(ctx: CanvasRenderingContext2D) {
-            ctx.translate(this.x, this.y);
-            ctx.rotate(this.angle);
-        }
-    }
-}
