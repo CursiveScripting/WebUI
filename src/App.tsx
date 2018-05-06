a600037c696a49918ab34ebb7e61afce8dd90922
FTWinston <ftwinston@gmail.com>
2018-05-06 16:48:06 +0100

    Moved workspace loading into Workspace class. Now loading sample workspace from XML.

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,51 @@
 import * as React from 'react';
-import './App.css';
 import { ProcessEditor } from './ui/ProcessEditor';
-import { StartStep, StopStep, Type, Parameter, ProcessStep, SystemProcess, UserProcess, Workspace } from './data';
+import { Workspace } from './data';
+import './App.css';
 
-const workspace = new Workspace();
+interface AppState {
+    workspace?: Workspace;
+}
 
-const stringType = new Type('text', '#00cc00', undefined, /.*/, 'Any old text');
-const numberType = new Type('number', '#0099ff', undefined, /^\d*$/, 'A number');
-const boolType = new Type('boolean', '#cc6600', undefined, undefined, 'true or false');
-workspace.types.add(stringType.name, stringType);
-workspace.types.add(numberType.name, numberType);
-workspace.types.add(boolType.name, boolType);
+class App extends React.PureComponent<{}, AppState> {
+    constructor(props: {}) {
+        super(props);
+        this.state = {};
+    }
 
-const parentProcess = new UserProcess(
-    'Example parent',
-    [new Parameter('Parent input 1', stringType), new Parameter('Parent input 2', numberType)],
-    [new Parameter('Parent output', boolType)],
-    [],
-    ['Yea', 'Nay'],
-    true,
-    'Some parent process to do something',
-    ''
-);
-workspace.userProcesses.add(parentProcess.name, parentProcess);
+    componentWillMount() {
+        let request = new XMLHttpRequest();
+        request.open('GET', 'workspace.xml', true);
+    
+        request.onload = () => {
+            if ((request.status >= 400 || request.status < 200) && request.status !== 0 || request.responseXML === null) {
+                throw 'Failed to load workspace XML';
+            }
 
-const systemProcess = new SystemProcess(
-    'System process',
-    [],
-    [],
-    [],
-    'An example process that doesn\'t really exist',
-    'Root',
-);
-workspace.systemProcesses.add(systemProcess.name, systemProcess);
+            this.setState({
+                workspace: Workspace.loadWorkspace(request.responseXML),
+            });
+        };
+        request.send();
+    }
 
-const userProcess = new UserProcess(
-    'User process',
-    [new Parameter('Input 1', stringType), new Parameter('Input 2', numberType)],
-    [new Parameter('Output', boolType)],
-    [],
-    ['Yea', 'Nay'],
-    true,
-    'Some process to do something',
-    ''
-);
-workspace.userProcesses.add(userProcess.name, userProcess);
+    render() {
+        if (this.state.workspace === undefined) {
+            return <div>Please wait</div>;
+        }
+      
+        return (
+            <ProcessEditor
+                className="fullScreen"
+                workspace={this.state.workspace}
+            />
+        );
+    }
 
-const startStep = new StartStep(1, parentProcess, 32, 48);
-const stopStep = new StopStep(2, parentProcess, 'Yea', 800, 256);
-const systemStep = new ProcessStep(3, systemProcess, parentProcess, 176, 48);
-const userStep = new ProcessStep(4, userProcess, parentProcess, 448, 64);
-
-parentProcess.steps.push(startStep, stopStep, systemStep, userStep);
-
-class App extends React.Component {
-  render() {
-    return <ProcessEditor className="fullScreen" workspace={workspace} initialProcess={parentProcess} />;
-  }
+    private saveProcesses(xml: XMLDocument) {
+        console.log('not really saving...', xml);
+        return true;
+    }
 }
 
 export default App;
