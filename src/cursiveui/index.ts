a29c621370000a939578b566174d710533b2c964
FTWinston <ftwinston@gmail.com>
2019-09-12 22:52:50 +0100

    Default export now includes some common interfaces

--- src/cursiveui/index.ts
+++ src/cursiveui/index.ts
@@ -1,9 +1,9 @@
 import { CursiveUI } from './CursiveUI';
-
-/*
+import { IWorkspaceData, IUserProcessData } from './services/serializedDataModels';
 import { ICustomTool } from './ICustomTool';
 
 export type ICustomTool = ICustomTool;
-*/
+export type IWorkspaceData = IWorkspaceData;
+export type IUserProcessData = IUserProcessData;
 
 export default CursiveUI;
